368e5177b493cb5abaf724cbce925356833587c5
lvlupJamie <aeboltt@gmail.com>
2026-04-17 11:52:37 +0200

    Combat Basics and Enemy Movement AI

---
+++ HigherThanRent/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    //Enemy AI
+    public GameObject Player;
+    public float speed;
+
+    private float distance;
+    public float distanceBetween;
+
+    //Combat
+    public int maxHealth = 3;
+    int currentHealth;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        //Enemy Die Animation Goes Here!!
+
+        //Disable the enemy 
+        GetComponent<Collider2D>().enabled = false;
+        this.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector2.Distance(transform.position, Player.transform.position);
+
+        if (distance < distanceBetween)
+        {
+            transform.position = Vector2.MoveTowards(this.transform.position, Player.transform.position, speed * Time.deltaTime);
+        }
+
+        /*if (distanceBetween < 1)
+        {
+            remove 1 hp from player (need to establish hp for player in another script assigned to player - must be public)
+            give time until enemy can attack again
+        }
+        */
+    }
+}
---
+++ HigherThanRent/Assets/Scripts/PlayerCombat.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    public Transform attackPoint;
+
+    public float attackRange = 2f;
+
+    public int attackDamage = 1;
+
+    //How many times you can attack per second
+    public float attackRate = 2f;
+    float nextAttackTime = 0f;
+
+    //Defines which objects are enemies, only attacks objects detected in this layer 
+    public LayerMask enemyLayers;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Time.time >= nextAttackTime)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Attack();
+                nextAttackTime = Time.time + 1f / attackRate;
+            }
+        }
+    }
+    void Attack()
+    {
+        //Attack Aimation Goes Here!!
+        
+        //Detect Enemies in range
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        //Damage Enemies
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<EnemyAI>().TakeDamage(attackDamage);
+        }
+    }
+
+    //View attack Range in unity editor
+    void onDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+}
