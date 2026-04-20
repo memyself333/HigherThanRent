92485b39df6f4f96da59d4713a899e0750c9ae73
VescentKith <brat_style@hotmail.com>
2026-04-20 12:49:57 +0200

    Save Button Edits
    - Save Button only appears when in range of Sophie
    - Changed dialogue sprite for Fox

--- HigherThanRent/Assets/Scripts/NPCScripts/NPCStates/NPCTalk.cs
+++ HigherThanRent/Assets/Scripts/NPCScripts/NPCStates/NPCTalk.cs
@@ -1,11 +1,14 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NPCTalk : MonoBehaviour
 {
     private Animator anim; // The NPC animator
     public Animator interactAnim; // The Interact Icon animator
     public DialogueSO dialogueSO; // What conversation this will have
+
+    public GameObject saveButton; // Gonna try and get the Save Button to toggle on and off
 
     private void Awake()
     {
@@ -16,11 +19,13 @@ public class NPCTalk : MonoBehaviour
     {
         anim.Play("Idle"); // Set NPC animation to idle while talking to NPC
         interactAnim.Play("Open"); 
+        saveButton.SetActive(true); // Trying to turn on the Save Button
     }
 
     private void OnDisable() // When the script is toggled off
     {
         interactAnim.Play("Close");
+        saveButton.SetActive(false); // Trying to turn off the Save Button
     }
 
     private void Update()
