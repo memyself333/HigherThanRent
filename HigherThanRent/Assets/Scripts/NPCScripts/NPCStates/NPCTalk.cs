cbb92b899b35a9898f3cbb33a84ea674e8ece682
memyself333 <135866389+memyself333@users.noreply.github.com>
2026-04-20 17:45:29 +0200

    Fixed Bugs
    - fixed enemy insta killing
    - fixed save button not reappearing after changing scenes

--- HigherThanRent/Assets/Scripts/NPCScripts/NPCStates/NPCTalk.cs
+++ HigherThanRent/Assets/Scripts/NPCScripts/NPCStates/NPCTalk.cs
@@ -9,27 +9,20 @@ public class NPCTalk : MonoBehaviour
     public Animator interactAnim; // The Interact Icon animator
     public DialogueSO dialogueSO; // What conversation this will have
 
-    public GameObject saveButton; // Gonna try and get the Save Button to toggle on and off
-
     private void Awake()
     {
-        saveButton = GameObject.FindGameObjectWithTag("SaveButton");
         anim = GetComponentInChildren<Animator>(); // Accessing the animator, which is in a child object
-    
-        
     }
 
     private void OnEnable() // When the script is toggled on
-    {
+    { 
         anim.Play("Idle"); // Set NPC animation to idle while talking to NPC
         interactAnim.Play("Open"); 
-        saveButton.SetActive(true); // Trying to turn on the Save Button
     }
 
     private void OnDisable() // When the script is toggled off
     {
         interactAnim.Play("Close");
-        saveButton.SetActive(false); // Trying to turn off the Save Button
     }
 
     private void Update()
