cfeeb99e69786faa9e1cbf36e8ca59c71ea7b09a
memyself333 <135866389+memyself333@users.noreply.github.com>
2026-05-01 15:25:53 +0200

    Started Work On Main Menu
    - changed and added main menu things

--- HigherThanRent/Assets/Scripts/Menus/MainMenu.cs
+++ HigherThanRent/Assets/Scripts/Menus/MainMenu.cs
@@ -1,9 +1,22 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private string gameSceneName;
+    
+    public Image startButton;
+    public Image loadButton;
+    public Image quitButton;
+
+    private void Start()
+    {
+        startButton.alphaHitTestMinimumThreshold = 0.1f;
+        loadButton.alphaHitTestMinimumThreshold = 0.1f;
+        quitButton.alphaHitTestMinimumThreshold = 0.1f;
+    }
+
 
     public void Play()
     {
